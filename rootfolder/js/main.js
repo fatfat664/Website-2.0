6f8f81073e7b5c636373901c7e273a4a2d1fd46a
Agnibh Dasgupta <44358874+fatfat664@users.noreply.github.com>
2020-06-01 11:55:26 -0600

    Add files via upload

---
+++ rootfolder/js/main.js
@@ -0,0 +1,46 @@
+// Trying variables
+var num1 = 69;
+var kek = "nice";
+
+// Trying constants
+const NAME = "Niv";
+
+// If, alert, space
+if (num1 == 69)
+{
+	alert(num1 + " " +  kek + " " + NAME);
+}
+
+//Testing
+document.body.innerHTML = "Boom!";
+
+//Typing into console
+const KEK = "Pog\"You\"";
+console.log(KEK);
+var a = 10;
+var b = 20;
+console.log(a + b);
+
+//Data types
+
+var int = 2066878734354; // int
+var float = 776846843.46877357; // float
+float *= -1; // negative float
+var string = "Agnibh"; // string
+var bool1 = true; //bool
+var bool2 = false;
+var bool3 = 1;
+var und; //undefined
+var undnull = null; //null
+
+
+// if, else
+if (bool3 & !(bool1 || bool2))
+{
+	console.log(int, float, string, bool1, bool2, bool3, und, undnull);
+	console.log(typeof(int), typeof(float), typeof(string), typeof(bool1), typeof(bool2), typeof(bool3), typeof(und), typeof(undnull));
+}
+else if (bool3)
+{
+	console.log(bool1)
+}
